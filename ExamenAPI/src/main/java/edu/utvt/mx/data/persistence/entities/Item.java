8b971b6dec08cc1b9791513cfa02c9780ba50bec
AlvarezHernandezSaul <al222110813@gmail.com>
2024-07-02 17:08:05 -0600

    Creacion de la entidades Item y Persona con su contenido

--- ExamenAPI/src/main/java/edu/utvt/mx/data/persistence/entities/Item.java
+++ ExamenAPI/src/main/java/edu/utvt/mx/data/persistence/entities/Item.java
@@ -1,5 +1,27 @@
 package edu.utvt.mx.data.persistence.entities;
 
-public class Item {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "items")
+public class Item {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "persona_id")
+    private Persona persona;
 }
--- ExamenAPI/src/main/java/edu/utvt/mx/data/persistence/entities/Persona.java
+++ ExamenAPI/src/main/java/edu/utvt/mx/data/persistence/entities/Persona.java
@@ -1,5 +1,29 @@
 package edu.utvt.mx.data.persistence.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "personas")
 public class Persona {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String email;
+
+    @OneToMany(mappedBy = "persona", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Item> items;
 }
+
