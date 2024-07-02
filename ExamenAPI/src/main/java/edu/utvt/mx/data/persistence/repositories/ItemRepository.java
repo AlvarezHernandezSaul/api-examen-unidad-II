06892896f34c6b56ed35fed11b6278ebbe9ba6cf
AlvarezHernandezSaul <al222110813@gmail.com>
2024-07-02 17:12:10 -0600

    Contenido en repositorios

--- ExamenAPI/src/main/java/edu/utvt/mx/data/persistence/repositories/ItemRepository.java
+++ ExamenAPI/src/main/java/edu/utvt/mx/data/persistence/repositories/ItemRepository.java
@@ -1,7 +1,9 @@
 package edu.utvt.mx.data.persistence.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import edu.utvt.mx.data.persistence.entities.Item;
+import java.util.List;
 
-public interface ItemRepository extends JpaRepository<T, ID> {
-
+public interface ItemRepository extends JpaRepository<Item, Long> {
+    List<Item> findByName(String name);
 }
--- ExamenAPI/src/main/java/edu/utvt/mx/data/persistence/repositories/PersonaRepository.java
+++ ExamenAPI/src/main/java/edu/utvt/mx/data/persistence/repositories/PersonaRepository.java
@@ -2,6 +2,10 @@ package edu.utvt.mx.data.persistence.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PersonaRepository extends JpaRepository<T, ID> {
+import edu.utvt.mx.data.persistence.entities.Persona;
 
+import java.util.List;
+
+public interface PersonaRepository extends JpaRepository<Persona, Long> {
+    List<Persona> findByName(String name);
 }
