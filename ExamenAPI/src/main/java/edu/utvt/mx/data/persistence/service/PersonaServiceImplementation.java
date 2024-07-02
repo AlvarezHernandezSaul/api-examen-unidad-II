1b849bdff0376a5cf19ece8294c8a27df3ab51a6
AlvarezHernandezSaul <al222110813@gmail.com>
2024-07-02 17:36:45 -0600

    Implementacion de PersonaServiceImplementation

--- ExamenAPI/src/main/java/edu/utvt/mx/data/persistence/service/PersonaServiceImplementation.java
+++ ExamenAPI/src/main/java/edu/utvt/mx/data/persistence/service/PersonaServiceImplementation.java
@@ -1,5 +1,47 @@
 package edu.utvt.mx.data.persistence.service;
 
-public class PersonaServiceImplementation {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import edu.utvt.mx.data.persistence.entities.Persona;
+import edu.utvt.mx.data.persistence.repositories.PersonaRepository;
+
+@Service
+public class PersonaServiceImplementation implements PersonaService {
+
+	@Autowired
+	private PersonaRepository personaRepository;
+
+	@Override
+	public List<Persona> getAllPersonas() {
+		return personaRepository.findAll();
+	}
+
+	@Override
+	public Optional<Persona> getPersonaById(Long id) {
+		return personaRepository.findById(id);
+	}
+
+	@Override
+	public List<Persona> getPersonasByName(String name) {
+		return personaRepository.findByName(name);
+	}
+
+	@Override
+	public Persona savePersona(Persona persona) {
+		return personaRepository.save(persona);
+	}
+
+	@Override
+	public void deletePersona(Long id) {
+		// TODO Auto-generated method stub
+		personaRepository.deleteById(id);
+	}
+	
+
+	
+	
 }
