1b3066ebd43714e7b57c0dfad0edd477123defed
AlvarezHernandezSaul <al222110813@gmail.com>
2024-07-02 17:27:59 -0600

    Implementacion de ItemServiceImplementation

--- ExamenAPI/src/main/java/edu/utvt/mx/data/persistence/service/ItemServiceImplementation.java
+++ ExamenAPI/src/main/java/edu/utvt/mx/data/persistence/service/ItemServiceImplementation.java
@@ -1,5 +1,46 @@
 package edu.utvt.mx.data.persistence.service;
 
-public class ItemServiceImplementation {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import edu.utvt.mx.data.persistence.entities.Item;
+import edu.utvt.mx.data.persistence.repositories.ItemRepository;
+
+@Service
+public class ItemServiceImplementation implements ItemService {
+
+	@Autowired
+	private ItemRepository itemRepository;
+	
+	@Override
+	public List<Item> getAllItems() {
+		return itemRepository.findAll();
+	}
+
+	@Override
+	public Optional<Item> getItemById(Long id) {
+		 return itemRepository.findById(id);
+	}
+
+	@Override
+	public List<Item> getItemsByName(String name) {
+        return itemRepository.findByName(name);
+
+	}
+
+	@Override
+	public Item saveItem(Item item) {
+        return itemRepository.save(item);
+			}
+
+	@Override
+	public void deleteItem(Long id) {
+		itemRepository.deleteById(id);		
+	}
+	
+	
+	
 }
